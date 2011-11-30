5f09b9925f2bbf3aff8b0d939122109e1ba0f780
ggzaery@gmail.com <ggzaery@gmail.com@ea3af53b-ff70-7f45-b37a-e118d7398a5e>
2011-11-30 05:38:40 +0000

    removed map generator copy. added nick's groups chat client.

---
+++ src/mediator/Mediator.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package mediator;
+
+/**
+ * Represents the Mediator interface.
+ * @author Nick Iannone
+ * @version 1.0 5/3/11
+ */
+public interface Mediator {
+
+	/**
+	 * Sends a message to the connected clients.
+	 * @param sender The sender of the message.
+	 * @param msg The message to be sent.
+	 */
+	public void sendMessage(Colleague sender, String msg);
+	
+	/**
+	 * Adds a colleague to the mediator.
+	 * @param col The colleague to attach.
+	 * @return <b>true</b> if the colleague was added properly; <b>false</b> otherwise.
+	 */
+	public boolean addColleague(Colleague col);
+	
+	/**
+	 * Removes a colleague from the mediator.
+	 * @param col The colleague to detach.
+	 * @return <b>true</b> if the colleague was removed properly; <b>false</b> otherwise.
+	 */
+	public boolean removeColleague(Colleague col);
+}
---
+++ src/mediator/bot/BotClient.java
@@ -0,0 +1,49 @@
+/**
+ * File Name: BotClient.java
+ * Author:    Maximilian Thomas Witte
+ * Date:      May 16, 2011
+ * Project:
+ *
+**/
+
+package mediator.bot;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maximilian Thomas Witte
+ * @version 1.00
+ */
+public class BotClient {
+
+    private static InetAddress ip;
+    private static final int port = 4000;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+            //Prompt the user for the address of the server
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter an address to connect to:");
+            String ipString = scan.nextLine();
+
+            //Parse that to an InetAddress
+            try {
+                    ip = InetAddress.getByName(ipString);
+                    new Bot("Bot1",ip,port).runMe();
+                    new Bot("Bot2",ip,port).runMe();
+                    System.out.println("Successfully infiltrated");
+            } catch (UnknownHostException e) {
+                    System.out.println("Could not parse address " + ipString + " into a valid host");
+                    System.exit(0);
+            }
+
+
+            
+    }
+}
